0030fe56abe97f68a33d8bafcba476641339cc72
TwanMulder <twan.mulder@gmail.com>
2020-07-14 15:16:17 +0200

    Add router to project

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function Router() {
+  return ( 
+    <h1>Dit is de router</h1>
+  )
+}
+
+export default Router
