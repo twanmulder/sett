b1b870135cc7081194f9898cf4d78751f45f4ad1
TwanMulder <twan.mulder@gmail.com>
2020-07-16 17:27:20 +0200

    Add scroll to top function in router

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,23 @@
-import React from "react"
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
+import React, { useEffect } from "react"
+import { BrowserRouter, Switch, Route, Redirect, useLocation } from "react-router-dom"
 
 import App from "./pages/App/App"
 import Home from "./pages/Home/Home"
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function Router() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Switch>
         <Route exact path="/">
           <Home />
