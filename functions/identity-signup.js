7a2b6e6cbad1409b51c20ebce1201c83d5f5292b
TwanMulder <twan.mulder@gmail.com>
2020-09-03 23:34:16 +0200

    remove livemode param from stripe webhook

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   const { user } = JSON.parse(event.body)
 
   // create a new customer in Stripe
-  const customer = await stripe.customers.create({ email: user.email, livemode: true })
+  const customer = await stripe.customers.create({ email: user.email })
 
   // subscribe the new customer to the free plan
   await stripe.subscriptions.create({
