45c0b018ac0c93c81a25da580a3c7f8132959232
TwanMulder <twan.mulder@gmail.com>
2020-09-08 19:03:11 +0200

    start working on mobile nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, useLocation, useHistory } from "react-router-dom"
 import netlifyIdentity from "netlify-identity-widget"
 
 import "./Header.scss"
 
 function Header(props) {
+  const [isNavOpen, updateNavOpenState] = useState(false)
+
   const user = props.user
 
   let isOnAppPage = false
@@ -26,66 +28,113 @@ function Header(props) {
     history.push("/")
   }
 
+  const handleToggleNav = () => {
+    updateNavOpenState(!isNavOpen)
+  }
+
   return (
-    <header>
+    <header className={isNavOpen ? "is-active" : null}>
       <nav>
-        <Link to="/">SETT</Link>
+        <div className="main-nav__bar">
+          <div className="main-nav__controls">
+            <button onClick={handleToggleNav} aria-expanded="false" className="main-nav__toggle" id="main-nav-toggle" title="Toggle Menu" aria-controls="main-nav-menu">
+              <span className="main-nav__toggle__icon">
+                <span className="main-nav__toggle__icon__line main-nav__toggle__icon__line--top"></span>
+                <span className="main-nav__toggle__icon__line main-nav__toggle__icon__line--bottom"></span>
+              </span>
+              <span className="visuallyhidden">Menu</span>
+            </button>
+          </div>
+          <div className="main-nav__logo">
+            <Link to="/">SETT</Link>
+          </div>
+          <ul className="main-nav__persistent">
+            <li className="main-nav__item">
+              {/* Show log in button when user is not logged in */}
+              {!user.isLoggedIn && (
+                <button onClick={login} className="button">
+                  Log in
+                </button>
+              )}
+            </li>
+          </ul>
+        </div>
 
-        {/* Show log in button when user is not logged in */}
-        {!user.isLoggedIn && (
-          <button onClick={login} className="button button--text">
-            Log in
-          </button>
-        )}
+        <div className="main-nav__menu">
+          <ul>
+            {/* Show log in button when user is not logged in */}
+            {!user.isLoggedIn && (
+              <li>
+                <button onClick={login} className="button button--text">
+                  Log in
+                </button>
+              </li>
+            )}
 
-        {/* Show sign up button when user is not logged in */}
-        {!user.isLoggedIn && (
-          <button onClick={signup} className="button button--text">
-            Sign up
-          </button>
-        )}
+            {/* Show sign up button when user is not logged in */}
+            {!user.isLoggedIn && (
+              <li>
+                <button onClick={signup} className="button button--text">
+                  Sign up
+                </button>
+              </li>
+            )}
 
-        {/* Show log out button when user is not logged in */}
-        {user.isLoggedIn && (
-          <button onClick={logout} className="button button--text">
-            Log out
-          </button>
-        )}
+            {/* Show log out button when user is not logged in */}
+            {user.isLoggedIn && (
+              <li>
+                <button onClick={logout} className="button button--text">
+                  Log out
+                </button>
+              </li>
+            )}
 
-        {/* Show demo button when user is not pro */}
-        {!user.isPro && !isOnDemoPage && (
-          <Link to="/demo" className="button button--ghost">
-            Free demo
-          </Link>
-        )}
+            {/* Show demo button when user is not pro */}
+            {!user.isPro && !isOnDemoPage && (
+              <li className="large-button">
+                <Link to="/demo" className="button button--ghost">
+                  Free demo
+                </Link>
+              </li>
+            )}
 
-        {/* Show go pro button when user is not pro and is logged in that starts login */}
-        {!user.isPro && !user.isLoggedIn && (
-          <button onClick={login} className="button">
-            Go pro
-          </button>
-        )}
+            {/* Show go pro button when user is not pro and is logged in that starts login */}
+            {!user.isPro && !user.isLoggedIn && (
+              <li className="large-button">
+                <button onClick={login} className="button">
+                  Go pro
+                </button>
+              </li>
+            )}
 
-        {/* Show go pro button when user is not pro and is logged in that links to stripe */}
-        {!user.isPro && user.isLoggedIn && (
-          <a href={user.stripePortalHref} className="button">
-            Go pro
-          </a>
-        )}
+            {/* Show go pro button when user is not pro and is logged in that links to stripe */}
+            {!user.isPro && user.isLoggedIn && (
+              <li className="large-button">
+                <a href={user.stripePortalHref} className="button">
+                  Go pro
+                </a>
+              </li>
+            )}
 
-        {/* Show manage subscription button when user is logged in and pro */}
-        {user.isPro && user.isLoggedIn && (
-          <a href={user.stripePortalHref} className="button button--ghost">
-            Manage subscription
-          </a>
-        )}
+            {/* Show manage subscription button when user is logged in and pro */}
+            {user.isPro && user.isLoggedIn && (
+              <li className="large-button">
+                <a href={user.stripePortalHref} className="button button--ghost">
+                  Manage subscription
+                </a>
+              </li>
+            )}
 
-        {/* Show app button when user is logged in and pro */}
-        {user.isPro && user.isLoggedIn && !isOnAppPage && (
-          <Link className="button" to="/app">
-            Create bannerset
-          </Link>
-        )}
+            {/* Show app button when user is logged in and pro */}
+            {user.isPro && user.isLoggedIn && !isOnAppPage && (
+              <li className="large-button">
+                <Link className="button" to="/app">
+                  Create bannerset
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
       </nav>
     </header>
   )
